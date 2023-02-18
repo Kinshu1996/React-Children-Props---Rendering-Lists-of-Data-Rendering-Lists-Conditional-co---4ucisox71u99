957540222dc7ad080a82c389b110bf8f3f46f934
Kinshu1996 <ankitagrawal678y@gmail.com>
2023-02-18 18:51:36 +0530

    find the square

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,18 @@ import '../styles/App.css';
 import ChildComponent from './ChildComponent';
 const App = () => {
  //code here
+ const[number,setNumber] = useState(0);
+ const updateNumber = (e) => {
+  let num = e.target.value && parseInt(e.target.value, 10) > 0 ? parseInt(e.target.value,10) : 0;
+  setNumber(num);
+ }
 
   return (
     <div id="main">
       <ChildComponent /> 
-       <input id='input' onChange={} />
+       <input id='input' onChange={updateNumber} />
     
-      <p id='output'>{}</p>
+      <p id='output'>{number * number}</p>
     </div>
   )
 }
